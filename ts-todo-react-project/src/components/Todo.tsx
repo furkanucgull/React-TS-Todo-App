ec669f40c8ccfbc924fcafb737913196eb5741b7
furkanucgull <furkanucgull@gmail.com>
2024-07-21 15:27:28 +0200

    deletefunc-added

--- ts-todo-react-project/src/components/Todo.tsx
+++ ts-todo-react-project/src/components/Todo.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { IoIosRemoveCircleOutline } from "react-icons/io";
-import { FaCheck } from "react-icons/fa";
-import { MdOutlineModeEdit } from "react-icons/md";
+import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById } from '../redux/todoSlice';
 
-function Todo() {
+interface TodoProps {
+    todoProps: TodoType;
+}
+
+function Todo({ todoProps }: TodoProps) {
+    const { id, content } = todoProps;
+    const dispatch = useDispatch();
+
     return (
         <div className='flex flex-row items-center justify-center mt-12'>
 
-            <div className=' w-6/12 flex flex-row justify-between items-center'>
+            <div className=' w-6/12 flex flex-row justify-between items-center p-5 border border-gray-400 rounded-xl'>
                 <div className='  '>
-                    First Todo....
+                    {content}
                 </div>
                 <div className='icons flex flex-row gap-4'>
-                    <IoIosRemoveCircleOutline size={25} className='cursor-pointer' />
-                    <MdOutlineModeEdit size={25} className='cursor-pointer' />
+                    <IoIosRemoveCircleOutline
+                        onClick={() => dispatch(removeTodoById(id))}
+                        size={25} className='cursor-pointer' />
+
                 </div>
 
             </div>
